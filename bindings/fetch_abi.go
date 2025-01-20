c56268e71ecebe7d65dcdc86ffcab2ee113b0f22
Nite5ky <aakashyadav@lampros.tech>
2025-01-20 09:55:20 +0530

    feat: updated bindinga to v1.0.4

--- bindings/fetch_abi.go
+++ bindings/fetch_abi.go
@@ -1,66 +1,67 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "context"
-    "path/filepath"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
 
-    "github.com/ethereum/go-ethereum/ethclient"
-    "github.com/ethereum/go-ethereum/common"
-    "github.com/joho/godotenv"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
 )
 
 type ContractAddresses struct {
-    Proxy          string `json:"proxy"`
-    Implementation string `json:"implementation"`
+	Proxy          string `json:"proxy"`
+	Implementation string `json:"implementation"`
 }
 
 type TriggerXDeploymentConfig struct {
-    RegistryCoordinator   ContractAddresses `json:"registryCoordinator"`
-    OperatorStateRetriever string            `json:"operatorStateRetriever"`
-    TriggerXServiceManager ContractAddresses `json:"triggerXServiceManager"`
-    TriggerXTaskManager    ContractAddresses `json:"triggerXTaskManager"`
+	VetoableSlasher        ContractAddresses `json:"vetoableSlasher"`
+	RegistryCoordinator    ContractAddresses `json:"registryCoordinator"`
+	OperatorStateRetriever string            `json:"operatorStateRetriever"`
+	TriggerXServiceManager ContractAddresses `json:"triggerXServiceManager"`
+	TriggerXTaskManager    ContractAddresses `json:"triggerXTaskManager"`
 }
 
 type EigenlayerDeploymentConfig struct {
-    DelegationManager ContractAddresses `json:"delegationManager"`
-    AvsDirectory ContractAddresses `json:"avsDirectory"`
-    AllocationManager ContractAddresses `json:"allocationManager"`
-    PermissionController ContractAddresses `json:"permissionController"`
-    StrategyManager ContractAddresses `json:"strategyManager"`
+	DelegationManager    ContractAddresses `json:"delegationManager"`
+	AvsDirectory         ContractAddresses `json:"avsDirectory"`
+	AllocationManager    ContractAddresses `json:"allocationManager"`
+	PermissionController ContractAddresses `json:"permissionController"`
+	StrategyManager      ContractAddresses `json:"strategyManager"`
 }
 
 type StakeDeploymentConfig struct {
-    TriggerXStakeRegistry ContractAddresses `json:"triggerXStakeRegistry"`
+	TriggerXStakeRegistry ContractAddresses `json:"triggerXStakeRegistry"`
 }
 
 const (
-    OUTPUT_DIR = "bindings/abis"
+	OUTPUT_DIR = "bindings/abis"
 )
 
 func main() {
-    if err := godotenv.Load(); err != nil {
-        panic("Error loading .env file")
-    }
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file")
+	}
 
 	holeskyClient, err := ethclient.Dial(os.Getenv("HOLESKY_RPC_URL"))
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    opSepoliaClient, err := ethclient.Dial(os.Getenv("OP_SEPOLIA_RPC_URL"))
-    if err != nil {
-        panic(err)
-    }
+	opSepoliaClient, err := ethclient.Dial(os.Getenv("OP_SEPOLIA_RPC_URL"))
+	if err != nil {
+		panic(err)
+	}
 
-    triggerxHoleskyData, err := ioutil.ReadFile("./bindings/deployment.holesky.json")
-    if err != nil {
-        panic(err)
-    }
+	triggerxHoleskyData, err := ioutil.ReadFile("./bindings/deployment.holesky.json")
+	if err != nil {
+		panic(err)
+	}
 
 	eigenlayerHoleskyData, err := ioutil.ReadFile("./bindings/eigenlayer.holesky.json")
 	if err != nil {
@@ -72,84 +73,65 @@ func main() {
 		panic(err)
 	}
 
-    var txDeployments TriggerXDeploymentConfig
-    if err := json.Unmarshal(triggerxHoleskyData, &txDeployments); err != nil {
-        panic(err)
-    }
+	var txDeployments TriggerXDeploymentConfig
+	if err := json.Unmarshal(triggerxHoleskyData, &txDeployments); err != nil {
+		panic(err)
+	}
 
 	var eigenlayerDeployments EigenlayerDeploymentConfig
 	if err := json.Unmarshal(eigenlayerHoleskyData, &eigenlayerDeployments); err != nil {
 		panic(err)
 	}
 
-    var stakeDeployments StakeDeploymentConfig
-    if err := json.Unmarshal(stakeOPSData, &stakeDeployments); err != nil {
-        panic(err)
-    }
-
-    fetchBytecode := func(name string, address string, client *ethclient.Client, network string) {
-        binPath := filepath.Join(OUTPUT_DIR, name+"."+network+".bin")
-        if _, err := os.Stat(binPath); err == nil {
-            return
-        }
-
-        addr := common.HexToAddress(address)
-        bytecode, err := client.CodeAt(context.Background(), addr, nil)
-        if err != nil {
-            panic(err)
-        }
-
-        err = ioutil.WriteFile(
-            binPath,
-            []byte(fmt.Sprintf("%x", bytecode)),
-            0644,
-        )
-        if err != nil {
-            panic(err)
-        }
-        fmt.Printf("Fetched bytecode for %s on %s\n", name, network)
-    }
-
-    triggerxContracts := map[string]string{
-        "RegistryCoordinator":    txDeployments.RegistryCoordinator.Implementation,
-        "OperatorStateRetriever": txDeployments.OperatorStateRetriever,
-        "TriggerXServiceManager": txDeployments.TriggerXServiceManager.Implementation,
-        "TriggerXTaskManager":    txDeployments.TriggerXTaskManager.Implementation,
-    }
-
-	eigenlayerContracts := map[string]string{
-		"DelegationManager": eigenlayerDeployments.DelegationManager.Implementation,
-		"AvsDirectory": eigenlayerDeployments.AvsDirectory.Implementation,
+	var stakeDeployments StakeDeploymentConfig
+	if err := json.Unmarshal(stakeOPSData, &stakeDeployments); err != nil {
+		panic(err)
 	}
 
-    opSepoliaContracts := map[string]string{
-        "TriggerXStakeRegistry": stakeDeployments.TriggerXStakeRegistry.Implementation,
-    }
+	fetchBytecode := func(name string, address string, client *ethclient.Client, network string) {
+		binPath := filepath.Join(OUTPUT_DIR, name+"."+network+".bin")
+		if _, err := os.Stat(binPath); err == nil {
+			return
+		}
 
-    for name, addr := range triggerxContracts {
-        fetchBytecode(name, addr, holeskyClient, "holesky")
-        abi, err := fetchABIFromBlockscout(addr, "holesky")
-        if err != nil {
-            panic(err)
-        }
+		addr := common.HexToAddress(address)
+		bytecode, err := client.CodeAt(context.Background(), addr, nil)
+		if err != nil {
+			panic(err)
+		}
 
-        abiPath := filepath.Join(OUTPUT_DIR, name+".holesky.abi")
-        if _, err := os.Stat(abiPath); err == nil {
-            continue
-        }
+		err = ioutil.WriteFile(
+			binPath,
+			[]byte(fmt.Sprintf("%x", bytecode)),
+			0644,
+		)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Fetched bytecode for %s on %s\n", name, network)
+	}
 
-        err = ioutil.WriteFile(
-            abiPath,
-            []byte(abi),
-            0644,
-        )
-        if err != nil {
-            panic(err)
-        }
-        fmt.Printf("Fetched ABI for %s on holesky\n", name)
-    }
+	triggerxContracts := map[string]string{
+		"VetoableSlasher":        txDeployments.VetoableSlasher.Implementation,
+		"RegistryCoordinator":    txDeployments.RegistryCoordinator.Implementation,
+		"OperatorStateRetriever": txDeployments.OperatorStateRetriever,
+		"TriggerXServiceManager": txDeployments.TriggerXServiceManager.Implementation,
+		"TriggerXTaskManager":    txDeployments.TriggerXTaskManager.Implementation,
+	}
 
-	for name, addr := range eigenlayerContracts {
+	eigenlayerContracts := map[string]string{
+		"DelegationManager":    eigenlayerDeployments.DelegationManager.Implementation,
+		"AvsDirectory":         eigenlayerDeployments.AvsDirectory.Implementation,
+		"AllocationManager":    eigenlayerDeployments.AllocationManager.Implementation,
+		"PermissionController": eigenlayerDeployments.PermissionController.Implementation,
+		"StrategyManager":      eigenlayerDeployments.StrategyManager.Implementation,
+	}
+
+	opSepoliaContracts := map[string]string{
+		"TriggerXStakeRegistry": stakeDeployments.TriggerXStakeRegistry.Implementation,
+	}
+
+	for name, addr := range triggerxContracts {
 		fetchBytecode(name, addr, holeskyClient, "holesky")
 		abi, err := fetchABIFromBlockscout(addr, "holesky")
 		if err != nil {
@@ -172,67 +154,136 @@ func main() {
 		fmt.Printf("Fetched ABI for %s on holesky\n", name)
 	}
 
-    for name, addr := range opSepoliaContracts {
-        fetchBytecode(name, addr, opSepoliaClient, "opsepolia")
-        abi, err := fetchABIFromBlockscout(addr, "opsepolia")
-        if err != nil {
-            panic(err)
-        }
+	for name, addr := range eigenlayerContracts {
+		fetchBytecode(name, addr, holeskyClient, "holesky")
+		abi, err := fetchABIFromEtherscan(addr, "holesky")
+		if err != nil {
+			panic(err)
+		}
 
-        abiPath := filepath.Join(OUTPUT_DIR, name+".opsepolia.abi")
-        if _, err := os.Stat(abiPath); err == nil {
-            continue
-        }
+		abiPath := filepath.Join(OUTPUT_DIR, name+".holesky.abi")
+		if _, err := os.Stat(abiPath); err == nil {
+			continue
+		}
 
-        err = ioutil.WriteFile(
-            abiPath,
-            []byte(abi),
-            0644,
-        )
-        if err != nil {
-            panic(err)
-        }
-        fmt.Printf("Fetched ABI for %s on opsepolia\n", name)
-    }
+		err = ioutil.WriteFile(
+			abiPath,
+			[]byte(abi),
+			0644,
+		)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Fetched ABI for %s on holesky\n", name)
+	}
+
+	for name, addr := range opSepoliaContracts {
+		fetchBytecode(name, addr, opSepoliaClient, "opsepolia")
+		abi, err := fetchABIFromBlockscout(addr, "opsepolia")
+		if err != nil {
+			panic(err)
+		}
+
+		abiPath := filepath.Join(OUTPUT_DIR, name+".opsepolia.abi")
+		if _, err := os.Stat(abiPath); err == nil {
+			continue
+		}
+
+		err = ioutil.WriteFile(
+			abiPath,
+			[]byte(abi),
+			0644,
+		)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Fetched ABI for %s on opsepolia\n", name)
+	}
+}
+
+func fetchABIFromEtherscan(address string, network string) (string, error) {
+	apiKey := os.Getenv("ETHERSCAN_API_KEY")
+	if apiKey == "" {
+		return "", fmt.Errorf("ETHERSCAN_API_KEY not set in environment")
+	}
+
+	var baseURL string
+	switch network {
+	case "holesky":
+		baseURL = "https://api-holesky.etherscan.io/api"
+	case "opsepolia":
+		baseURL = "https://api-optimistic.etherscan.io/api"
+	default:
+		return "", fmt.Errorf("unsupported network: %s", network)
+	}
+
+	url := fmt.Sprintf("%s?module=contract&action=getabi&address=%s&apikey=%s", baseURL, address, apiKey)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch ABI: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	var result struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Result  string `json:"result"`
+	}
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		return "", fmt.Errorf("failed to parse JSON response: %v", err)
+	}
+
+	if result.Status != "1" {
+		return "", fmt.Errorf("etherscan API error: %s", result.Message)
+	}
+
+	return result.Result, nil
 }
 
 func fetchABIFromBlockscout(address string, network string) (string, error) {
-    var baseURL string
-    switch network {
-    case "holesky":
-        baseURL = "https://eth-holesky.blockscout.com/api"
-    case "opsepolia":
-        baseURL = "https://optimism-sepolia.blockscout.com/api"
-    default:
-        return "", fmt.Errorf("unsupported network: %s", network)
-    }
+	var baseURL string
+	switch network {
+	case "holesky":
+		baseURL = "https://eth-holesky.blockscout.com/api"
+	case "opsepolia":
+		baseURL = "https://optimism-sepolia.blockscout.com/api"
+	default:
+		return "", fmt.Errorf("unsupported network: %s", network)
+	}
 
-    url := fmt.Sprintf("%s?module=contract&action=getabi&address=%s", baseURL, address)
-    
-    resp, err := http.Get(url)
-    if err != nil {
-        return "", fmt.Errorf("failed to fetch ABI: %v", err)
-    }
-    defer resp.Body.Close()
+	url := fmt.Sprintf("%s?module=contract&action=getabi&address=%s", baseURL, address)
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return "", fmt.Errorf("failed to read response body: %v", err)
-    }
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch ABI: %v", err)
+	}
+	defer resp.Body.Close()
 
-    var result struct {
-        Status  string `json:"status"`
-        Message string `json:"message"`
-        Result  string `json:"result"`
-    }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %v", err)
+	}
 
-    if err := json.Unmarshal(body, &result); err != nil {
-        return "", fmt.Errorf("failed to parse JSON response: %v", err)
-    }
+	var result struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Result  string `json:"result"`
+	}
 
-    if result.Status != "1" {
-        return "", fmt.Errorf("blockscout API error: %s ||| %s" , result.Message, address)
-    }
+	if err := json.Unmarshal(body, &result); err != nil {
+		return "", fmt.Errorf("failed to parse JSON response: %v", err)
+	}
 
-    return result.Result, nil
+	if result.Status != "1" {
+		return "", fmt.Errorf("blockscout API error: %s ||| %s", result.Message, address)
+	}
+
+	return result.Result, nil
 }
