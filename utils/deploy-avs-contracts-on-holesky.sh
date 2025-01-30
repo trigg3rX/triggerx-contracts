414691ec8172c7e2614da93547ab0f3648c444e4
Nite5ky <aakashyadav@lampros.tech>
2025-01-30 11:13:48 +0530

    feat: contracts redeployed

--- utils/deploy-avs-contracts-on-holesky.sh
+++ utils/deploy-avs-contracts-on-holesky.sh
@@ -9,7 +9,5 @@ forge script script/TriggerXDeployerHolesky.s.sol:TriggerXDeployerHolesky \
         --rpc-url $HOLESKY_RPC_URL \
         --private-key $TRIGGERX_OWNER_PRIVATE_KEY \
         --verify \
-        --verifier blockscout \
-        --verifier-url https://eth-holesky.blockscout.com/api/ \
-        --etherscan-api-key $BLOCKSCOUT_API_KEY \
+        --etherscan-api-key $ETHERSCAN_API_KEY \
         --broadcast -vvvv
